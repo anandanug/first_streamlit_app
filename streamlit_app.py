39f63b82d8ab1992c388a8d4f94ba1909962a60a
Ananda Anugrah Ramadhan <71080135+anandanug@users.noreply.github.com>
2023-09-14 16:07:35 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
-import streamlit as st
+import streamlitreamlit
 
-st.title("My Parents New Healthy Dinner")
+streamlit.title("My Parents New Healthy Dinner")
 
-st.header('Breakfast Menu')
-st.text('Omega 3 & Blueberry Oatmeal')
-st.text('Kale, Spinach & Rocket Smoothie')
-st.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfastreamlit Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
